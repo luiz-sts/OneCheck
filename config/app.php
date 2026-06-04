61b3868efa19bd70b250a2d3e1af6ebf6e43deb2
Luiz Felipe <doti0026@gmail.com>
2026-06-03 22:23:57 -0300

    Versao SIte

---
+++ config/app.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Configuração geral da aplicação.
+ * base_path: caminho web até a pasta onecheck (ajuste se necessário).
+ * Ex.: '/onecheck' para http://localhost/onecheck/
+ */
+return [
+    // Caminho no navegador: http://localhost/onecheck/
+    'base_path' => '/onecheck',
+    'name'      => 'OneCheck',
+];
