88c0571712b4a608e0977ca14ce9bb52052f555c
Luiz Felipe <doti0026@gmail.com>
2026-06-07 18:37:04 -0300

    Corrige base_path para deploy

---
+++ app.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Configuração geral da aplicação.
+ * base_path: caminho web até a pasta onecheck (ajuste se necessário).
+ * Ex.: '/onecheck' para http://localhost/onecheck/
+ */
+return [
+    'base_path' => '',
+    'name'      => 'OneCheck',
+];
